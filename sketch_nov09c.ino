8e4b68dcfd1d95630b88c1d43119d26f56b95c31
nickopupiales <30836910+nickopupiales@users.noreply.github.com>
2017-11-09 23:46:30 -0500

    PRUEBA ESP JUNTO CON SERVOMO
    Al tener la IP seleccionada junto con la respectiva interfaz, se procedió a unir el modulo ESP8266 junto a un servo motor SG90 el cual cumplia la funcion de abrir y cerrar la cerradura.
    El ESP8266 recibe la señal enviada desde el dispositivo conectado a la IP, y este a su vez envia una señal para que el servo motor abra o cierre la cerradura

---
+++ sketch_nov09c.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+
+Servo myservo; 
+
+void setup() {
+  pinMode(A2, INPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  myservo.attach(2,1000,2400); 
+}
+
+void loop() {
+  int V = analogRead(A2);
+  
+
+
+if(V < 500){
+  //CERRADURA ABIERTA
+   digitalWrite(13,HIGH); 
+   myservo.write(0);
+  }
+
+if(V > 500){
+  //CERRADURA CERRADA
+   digitalWrite(13,LOW);
+   myservo.write(180);
+  }
+
+  
+ }
